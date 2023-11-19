eed4da97d27c6a3df2e4e649a7a596231525610c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-11-19 01:51:07 +0100

    xun: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: xun Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_xun-user
+add_lunch_combo omni_xun-userdebug
+add_lunch_combo omni_xun-eng
